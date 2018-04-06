86d6820c1f4b20fe0898a9fdd0d7746981a7d625
DHRUV KUMAR <dkiphones620@gmail.com>
2018-04-06 12:38:43 +0530

    gen4

--- script/script.js
+++ script/script.js
@@ -1,16 +1,10 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 225, 255)",
-    "rgb(0, 0, 225)",
-    "rgb(255, 0, 255)"
-];
+var colors = generateRandomColor(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor()
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 =  document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,6 +18,7 @@ for (var i = 0; i < squares.length; i++) {
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;  
         }
         else {
             this.style.backgroundColor = "#232323";
@@ -38,7 +33,26 @@ function changeColors(color) {
     }
 }
 
-function pickColor(){
-   var random =  Math.floor((Math.random() * colors.length));
-   return colors[random];
+function pickColor() {
+    var random = Math.floor((Math.random() * colors.length));
+    return colors[random];
+}
+
+function generateRandomColor(num) {
+    var arr = [];
+    //add num random colors to array 
+    for (var i = 0; i < num; i++) {
+        //pushing num colors into the array
+        arr.push(randomColor())
+    }
+
+    return arr;
+}
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256)
+    var g = Math.floor(Math.random() * 256)
+    var b = Math.floor(Math.random() * 256)
+
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
